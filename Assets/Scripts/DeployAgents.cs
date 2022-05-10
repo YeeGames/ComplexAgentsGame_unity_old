4ccdc6cfae04c8c2285d206382991ce8262541f3
EthanSystem <ElementsPhoenix@outlook.com>
2022-05-10 11:49:33 +0800

    mod 2022-05-10 11:49:16
    重命名部署个体文件；

--- Assets/Scripts/DeployAgents.cs
+++ Assets/Scripts/DeployAgents.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using ComplexAgentsGame2D;
 using UnityEngine;
 
-public class DeployObjects : MonoBehaviour
+public class DeployAgents : MonoBehaviour
 {
+    
+    public Agent agent;
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.position = Random.insideUnitCircle * 50;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
