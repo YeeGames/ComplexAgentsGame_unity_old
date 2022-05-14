33c55e94bb95b8696a31bcead8d9cb3c082a9617
EthanSystem <ElementsPhoenix@outlook.com>
2022-05-14 22:33:43 +0800

    mod 2022-05-14 22:33:09
    尝试解决开启Trigger之后围墙不能碰撞的问题，失败。
    
    最后决定粒子部分暂时继续使用刚体碰撞，不开启Trigger。以避免围墙不能碰撞之问题。

--- Assets/Scripts/ComplexAgentsGame2D/Agents.cs
+++ Assets/Scripts/ComplexAgentsGame2D/Agents.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 using Random = UnityEngine.Random;
 
 // using ComplexAgentsGame2D;
@@ -11,22 +12,41 @@ namespace ComplexAgentsGame2D
     {
         public Agent agentPrefab;
         public GameSettings gameSettings;
-        public Color colour = Color.green;
+        public Color colour = Color.white;
         public AgentSettings agentSettings;
-        public float radiusSize = 30f;
+        public float radiusSize = 10f;
+        public int numAgent = 10;
 
         private void Awake()
         {
             // this.colour = gameSettings.colour;
             // Deploy agents
-            for (var i = 0; i < gameSettings.numOfAgent; i++)
+
+            // agentPrefab.set.type = AgentSettings.EType.Yang;
+            // agentPrefab.SetColor(Color.red);
+            for (var i = 0; i < numAgent; i++)
             {
                 Vector2 pos = (Vector2) (this.transform.position) + Random.insideUnitCircle * radiusSize;
-                Agent agent = Instantiate(agentPrefab);
-                agent.SetPosition(pos);
-                // Debug.Log("Position of agent " + i.ToString() + " is " + pos.ToString());
-                // agent.transform.forward = (Vector2) Random.insideUnitCircle;
-                agent.SetColor(this.colour);
+                Agent particleYang = Instantiate(agentPrefab);
+                particleYang.SetPosition(pos);
+                Debug.Log("Position of agent " + i.ToString() + " is " + pos.ToString());
+                particleYang.velocity = (Vector2) Random.insideUnitCircle;
+                particleYang.set.type = AgentSettings.EType.Yang;
+                particleYang.SetColor(Color.red);
+            }
+
+            // agentPrefab.set.type = AgentSettings.EType.Yin;
+            // agentPrefab.SetColor(Color.blue);
+            for (var i = 0; i < numAgent; i++)
+            {
+                Vector2 pos = (Vector2) (this.transform.position) + (Vector2) (this.transform.forward * radiusSize) +
+                              Random.insideUnitCircle * radiusSize;
+                Agent particleYin = Instantiate(agentPrefab);
+                particleYin.SetPosition(pos);
+                Debug.Log("Position of agent " + i.ToString() + " is " + pos.ToString());
+                particleYin.transform.forward = (Vector2) Random.insideUnitCircle;
+                particleYin.set.type = AgentSettings.EType.Yin;
+                particleYin.SetColor(Color.blue);
             }
         }
 
