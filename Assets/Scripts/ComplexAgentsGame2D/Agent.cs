7ab6cd10263ee6891bfeec987010b9ae8c8b51a4
EthanSystem <ElementsPhoenix@outlook.com>
2022-05-14 22:33:43 +0800

    mod 2022-05-14 22:33:09
    尝试解决开启Trigger之后围墙不能碰撞的问题，失败。
    
    最后决定粒子部分暂时继续使用刚体碰撞，不开启Trigger。以避免围墙不能碰撞之问题。

--- Assets/Scripts/ComplexAgentsGame2D/Agent.cs
+++ Assets/Scripts/ComplexAgentsGame2D/Agent.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditor.UIElements;
 using UnityEngine;
 using UnityEngine.Serialization;
 
@@ -9,9 +10,12 @@ namespace ComplexAgentsGame2D
 {
     public class Agent : MonoBehaviour
     {
-        public AgentSettings settings;
+        public AgentSettings set;
         public Team team;
         public Vector2 position;
+        public Vector2 velocity;
+        public Vector2 lastVelocity;
+        public float speed = 1f;
         public Vector2 forward;
 
         public float radius = 10f;
@@ -35,13 +39,15 @@ namespace ComplexAgentsGame2D
         public void Initialize(AgentSettings settings, Transform target)
         {
             // this.target = target;
-            this.settings = settings;
+            this.set = settings;
             this.transform.position = this.position;
         }
 
         void Awake()
         {
             spriteRenderer = transform.GetComponent<SpriteRenderer>();
+            velocity = new Vector2(0, 0) * speed;
+            Debug.Log("self's velocity is " + velocity.ToString());
         }
 
         // Start is called before the first frame update
@@ -52,17 +58,39 @@ namespace ComplexAgentsGame2D
         // Update is called once per frame
         void Update()
         {
-            transform.Translate(Vector2.zero * Time.deltaTime * settings.speed);
+            // transform.Translate(Vector2.zero * Time.deltaTime * set.speed);
         }
 
-        private void OnTriggerEnter(Collider other)
+        void LateUpdate()
         {
-            Debug.Log("触发触发器。");
+            lastVelocity = velocity;
         }
+
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+            if (other.gameObject.name == "Wall")
+            {
+                Debug.Log("碰撞到墙体");
+                Vector2 reflaexAngle = Vector2.Reflect(lastVelocity, other.GetContact(0).normal);
+                velocity = reflaexAngle.normalized * lastVelocity.magnitude;
+            }
+        }
+
+        // TODO HACK 构建失败
+        // private void OnTriggerEnter2D(Collider2D other)
+        // {
+        //     if (other.gameObject.name == "Wall")
+        //     {
+        //         Debug.Log("触发到墙体");
+        //         Vector2 contactPoint = other.bounds.ClosestPoint((Vector2) transform.position);
+        //         Vector2 reflaexAngle = Vector2.Reflect(lastVelocity, contactPoint.normal???);
+        //         velocity = reflaexAngle.normalized * lastVelocity.magnitude;
+        //     }
+        // }
 
         private void FixedUpdate()
         {
-            // throw new NotImplementedException();
+            transform.Translate(Vector2.zero * Time.deltaTime * set.speed);
         }
     }
 }
--- Assets/Scripts/ComplexAgentsGame2D/Force.cs
+++ Assets/Scripts/ComplexAgentsGame2D/Force.cs
@@ -1,9 +1,39 @@
+using System.Reflection;
 using UnityEngine;
 
 namespace ComplexAgentsGame2D
 {
     public class Force : MonoBehaviour
     {
+        // 【
+        // ————————————————
+        // 版权声明：本文为CSDN博主「Monkey_Xuan」的原创文章，遵循CC 4.0 BY-SA版权协议，转载请附上原文出处链接及本声明。
+        // 原文链接：https://blog.csdn.net/Monkey_Xuan/article/details/121118671
+
+        [HideInInspector] public Transform targetObject1;
+        [HideInInspector] public Transform targetImage1;
+        [HideInInspector] public float targetRange = 100; //大于等于0
+
+        void Start()
+        {
+            SetTargetImagePositon();
+        }
+
+        public void SetTargetImagePositon()
+        {
+            if (Vector3.Distance(targetObject1.position, Vector3.zero) >= targetRange)
+            {
+                targetObject1.position = Vector3.Normalize(targetObject1.position) * targetRange;
+            }
+
+            targetImage1.localPosition = targetObject1.position;
+        }
+        // 】
+        
+        // Vector3 reflexAngle=Vector3.Reflect
+
+        // TODO HACK 没有用到。【磁力，转载自网络，原链接忘了。
+
         public LayerMask m_MagneticLayers;
         public Vector3 m_Position;
         public float m_Radius;
@@ -31,5 +61,7 @@ namespace ComplexAgentsGame2D
             Gizmos.color = Color.red;
             Gizmos.DrawWireSphere(transform.position + m_Position, m_Radius);
         }
+
+        // 】
     }
 }
