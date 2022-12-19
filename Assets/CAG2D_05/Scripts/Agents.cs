0481efeea80e711a855836b7aa45a3f64efda8f1
Ethan Lin <ElementsPhoenix@outlook.com>
2022-12-19 17:18:12 +0800

    mod 2022-12-19 17:17:47
    尝试通过计算邻接矩阵方法修复粒子间交互错误；

--- Assets/CAG2D_05/Scripts/Agents.cs
+++ Assets/CAG2D_05/Scripts/Agents.cs
@@ -31,6 +31,7 @@ namespace CAG2D_05.Scripts
         {
             yeeType = YeeTypeChooser.ChooseYeeType(gameSettings.yeeFamilyEnum);
 
+            // 生成agent对象众
             for (var t = 0; t < yeeType.NumElement; t++) // 遍历每一类yeeType，以生成agent
             {
                 for (var i = 0; i < gameSettings.numAgent; i++) // 遍历单类yeeType之所有预定数量，以生成agent
@@ -52,49 +53,27 @@ namespace CAG2D_05.Scripts
                     // a.yeeRule = YeeTypeChooser.ChooseYeeRule(a.agentRuleEffector, gameSettings.yeeFamilyEnum);
                 }
             }
+            
+            //TODO 构建矩阵描述粒子之间之距离
+            
+            //TODO 构建矩阵描述粒子间是否有交互
+            //TODO 构建矩阵描述粒子间之力大小与方向
         }
 
-
-        // /// <summary>
-        // /// 使用非静态的YeeTypeChooser
-        // /// </summary>
-        // private void Awake()
-        // {
-        //     yeeType = _yeeTypeChooserNotStatics.ChooseYeeType(gameSettings.yeeFamilyEnum);
-        //
-        //     for (var t = 0; t < yeeType.NumElement; t++) // 遍历每一类yeeType，以生成agent
-        //     {
-        //         for (var i = 0; i < gameSettings.numAgent; i++) // 遍历单类yeeType之所有预定数量，以生成agent
-        //         {
-        //             Agent a = Agent.Instantiate(agent);
-        //
-        //             Vector2 pos = (Vector2) (this.transform.position) + Random.insideUnitCircle * radiusSize;
-        //             
-        //             a.aset.position = pos;
-        //             a.aset.velocity = Random.insideUnitCircle;
-        //             a.aset.id = i.ToString();
-        //             a.aset.YeeType = yeeType.YeeTypes[t];
-        //             a.aset.color = yeeType.Colors[t];
-        //             // a.aset.agentName = a.aset.agentBaseName + a.aset.YeeType + i.ToString();
-        //
-        //             a.Initialize(a.aset, a.rset);
-        //             
-        //             // a.agentRuleEffector = GameObject.Find("AgentRuleEffector").gameObject;
-        //             // a.yeeRule = YeeTypeChooser.ChooseYeeRule(a.agentRuleEffector, gameSettings.yeeFamilyEnum);
-        //
-        //         }
-        //     }
-        // }
 
         // Start is called before the first frame update
         void Start()
         {
+
         }
 
 
         // Update is called once per frame
         void FixedUpdate()
         {
+            //TODO 获取所有的粒子对象
+            
+            //
         }
     }
 }
